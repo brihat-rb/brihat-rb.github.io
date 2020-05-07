6cf927b92b1c3dc30faedb71748b161f1a816de5
brihat-rb <brihat.rb@gmail.com>
2020-05-07 14:00:30 +0545

    separate css file, date converter limit and base date change, some css update, restructure

--- nepali_calendar_tools.js
+++ nepali_calendar_tools.js
@@ -120,15 +120,15 @@ const LEAP_DAYS_LIST = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 const DAYS_LIST = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
 // very important
-const BASE_BS_YEAR = 2000
+const BASE_BS_YEAR = 1978
 const BASE_BS_MONTH = 1
 const BASE_BS_DATE = 1
 
-const BASE_AD_YEAR = 1943
+const BASE_AD_YEAR = 1921
 const BASE_AD_MONTH = 4
-const BASE_AD_DATE = 14
+const BASE_AD_DATE = 13
 
-const BASE_AD_OFFSET = 103
+const BASE_AD_OFFSET = 102
 
 const BS_MONTHS = ["Baisakh", "Jestha", "Ashad", "Shrawan", "Bhadra", "Ashwin", "Kartik", "Mangsir", "Poush", "Magh", "Falgun", "Chaitra"];
 const AD_MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -230,6 +230,16 @@ function convert_ad_to_bs(ad_year, ad_month, ad_date) {
     // input: ad_year, ad_month, ad_date - int
     // returns: tuple (bs_year, bs_month, bs_date)
 
+	if (ad_year < BASE_AD_YEAR || ad_year == BASE_AD_YEAR && ad_month < BASE_AD_MONTH || ad_year == BASE_AD_YEAR && ad_month == BASE_AD_MONTH && ad_date < BASE_BS_DATE) {
+		alert("Supported date range " + BASE_AD_YEAR + "-" + BASE_AD_MONTH + "-" + BASE_AD_DATE + " to 2036-4-14");
+		return;
+	}
+
+	if (ad_year > 2036 || ad_year == 2036 && ad_month > 4 || ad_year == 2036 && ad_month == 4 && ad_date > 14) {
+		alert("Date range 1921-4-13 to 2036-4-14");
+		return;
+	}
+
     let total_ad_days = 0
 
     for (let year = BASE_AD_YEAR; year < ad_year; year++) {
@@ -275,43 +285,46 @@ function convert_ad_to_bs(ad_year, ad_month, ad_date) {
     return res_bs_year + " " + BS_MONTHS[res_bs_month - 1] + " " + res_bs_date;
 }
 
-for(let year = 1978; year <= 2092; year++) {
-    let select = document.getElementById("select_year");
-    let option = document.createElement("option");
-    select.options.add(option);
-    option.text = year;
-    option.value = year;
-}
+select_list_ids = ["_", "_birthdate_", "_asof_"];
+for(let elem = 0; elem < 3; elem++) {
+  for(let year = 1978; year <= 2092; year++) {
+      let select = document.getElementById("select" + select_list_ids[elem] + "year");
+      let option = document.createElement("option");
+      select.options.add(option);
+      option.text = year;
+      option.value = year;
+  }
 
-for(let month = 1; month <= 12; month++) {
-    let select = document.getElementById("select_month");
-    let option = document.createElement("option");
-    select.options.add(option);
-    option.text = BS_MONTHS[month-1];
-    option.value = month;
-}
+  for(let month = 1; month <= 12; month++) {
+      let select = document.getElementById("select" + select_list_ids[elem] + "month");
+      let option = document.createElement("option");
+      select.options.add(option);
+      option.text = BS_MONTHS[month-1];
+      option.value = month;
+  }
 
-for(let date = 1; date <= 32; date++) {
-    let select = document.getElementById("select_date");
-    let option = document.createElement("option");
-    select.options.add(option);
-    option.text = date;
-    option.value = date;
+  for(let date = 1; date <= 32; date++) {
+      let select = document.getElementById("select" + select_list_ids[elem] + "date");
+      let option = document.createElement("option");
+      select.options.add(option);
+      option.text = date;
+      option.value = date;
+  }
 }
 
 function to_bs() {
     // shows converted BS date
-    ad_date_list = document.getElementById("ad_date").value.split("-");
-    result = convert_ad_to_bs(ad_date_list[0], ad_date_list[1], ad_date_list[2]);
+    let ad_date_list = document.getElementById("ad_date").value.split("-");
+    let result = convert_ad_to_bs(ad_date_list[0], ad_date_list[1], ad_date_list[2]);
     document.getElementById("bs_result").innerHTML = result;
 }
 
 function to_ad() {
     // shows converted AD date
-    bs_year = document.getElementById("select_year").value;
-    bs_month = document.getElementById("select_month").value;
-    bs_date = document.getElementById("select_date").value;
-    result = convert_bs_to_ad(bs_year, bs_month, bs_date);
+    let bs_year = document.getElementById("select_year").value;
+    let bs_month = document.getElementById("select_month").value;
+    let bs_date = document.getElementById("select_date").value;
+    let result = convert_bs_to_ad(bs_year, bs_month, bs_date);
     document.getElementById("ad_result").innerHTML = result;
 }
 
@@ -325,10 +338,115 @@ function get_bs_date_from_date_object(date) {
     return convert_ad_to_bs(date.getFullYear(), date.getMonth() + 1, date.getDate());
 }
 
+function get_ad_date_string(ad_date_result) {
+    ad_date_list = ad_date_result.split(" ");
+    ad_month = AD_MONTHS.indexOf(ad_date_list[1]) + 1
+    if (ad_month < 10) {
+      ad_month = "0" + ad_month;
+    }
+
+    if (ad_date_list[0] < 10) {
+      ad_date_list[0] = "0" + ad_date_list[0];
+    }
+    return ad_date_list[2] + "-" + ad_month + "-" + ad_date_list[0];
+}
+
+function date_diff() {
+    // computes date difference between two AD dates and displays in
+    // years months days
+    if (!document.getElementById("date2").value) {
+      document.getElementById('date2').value = new Date().toISOString().substr(0,10);
+    }
+    let date1 = new Date(document.getElementById("date1").value);
+    let date2 = new Date(document.getElementById("date2").value);
+
+    if (date1 > date2) {
+        let swap = date1;
+        date1 = date2;
+        date2 = swap;
+    }
+    let date1_year = date1.getFullYear();
+    let february = (date1_year % 4 === 0 && date1_year % 100 !== 0) || date1_year % 400 === 0 ? 29 : 28;
+    let days_list = [31, february, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    let no_of_years = date2.getFullYear() - date1_year;
+
+    let no_of_months = date2.getMonth() - date1.getMonth();
+    if (no_of_months < 0) {
+        no_of_years--;
+        no_of_months += 12;
+    }
+
+    let no_of_days = date2.getDate() - date1.getDate();
+    if (no_of_days < 0) {
+        if (no_of_months > 0) {
+            no_of_months--;
+        }
+        else {
+            no_of_years--;
+            no_of_months = 11;
+        }
+        no_of_days += days_list[date1.getMonth()];
+    }
+
+    let answer = '<span class="age_num">' + no_of_years + '</span> years';
+    answer += '&emsp;<span class="age_num">' + no_of_months + '</span> months';
+    answer += '&emsp;<span class="age_num">' + no_of_days + '</span> days';
+    document.getElementById("age_result").innerHTML = answer;
+}
+
+function date_diff_bs() {
+    // helper function, calls date_diff()
+    let birthdate_year = document.getElementById("select_birthdate_year").value;
+    let birthdate_month = document.getElementById("select_birthdate_month").value;
+    let birthdate_date = document.getElementById("select_birthdate_date").value;
+    let asofdate_year = document.getElementById("select_asof_year").value;
+    let asofdate_month = document.getElementById("select_asof_month").value;
+    let asofdate_date = document.getElementById("select_asof_date").value;
+
+    let ad_date_string = get_ad_date_string(convert_bs_to_ad(birthdate_year, birthdate_month, birthdate_date));
+    document.getElementById("date1").value = ad_date_string;
+
+    ad_date_string = get_ad_date_string(convert_bs_to_ad(asofdate_year, asofdate_month, asofdate_date));
+    document.getElementById("date2").value = ad_date_string;
+
+    date_diff();
+}
+
+function date_diff_ad() {
+    // helper function, calls date_diff()
+    if (!document.getElementById("date1").value) {
+      alert("Birthdate cannot be empty");
+      return;
+    }
+
+    let birthdate_ad = document.getElementById("date1").value;
+    let asofdate_ad = document.getElementById("date2").value;
+
+    let birthdate_ad_list = birthdate_ad.split("-");
+    let birthdate_bs_list = convert_ad_to_bs(birthdate_ad_list[0], birthdate_ad_list[1], birthdate_ad_list[2]).split(" ");
+    document.getElementById("select_birthdate_year").value = birthdate_bs_list[0];
+    document.getElementById("select_birthdate_month").value = get_bs_month(birthdate_bs_list[1]);
+    document.getElementById("select_birthdate_date").value = birthdate_bs_list[2];
+
+    let asofdate_ad_list = asofdate_ad.split("-");
+    let asofdate_bs_list = convert_ad_to_bs(asofdate_ad_list[0], asofdate_ad_list[1], asofdate_ad_list[2]).split(" ");
+    document.getElementById("select_asof_year").value = asofdate_bs_list[0];
+    document.getElementById("select_asof_month").value = get_bs_month(asofdate_bs_list[1]);
+    document.getElementById("select_asof_date").value = asofdate_bs_list[2];
+
+    date_diff();
+}
+
 // default settings on page load
-bs_today = get_bs_date_from_date_object(new Date()).split(" ");
+let bs_today = get_bs_date_from_date_object(new Date()).split(" ");
 document.getElementById("select_year").value = bs_today[0];
 document.getElementById("select_month").value = get_bs_month(bs_today[1]);
 document.getElementById("select_date").value = bs_today[2];
 
+document.getElementById("select_asof_year").value = bs_today[0];
+document.getElementById("select_asof_month").value = get_bs_month(bs_today[1]);
+document.getElementById("select_asof_date").value = bs_today[2];
+
 document.getElementById("ad_date").value = new Date().toISOString().substring(0,10);
+document.getElementById('date2').value = new Date().toISOString().substr(0,10);
