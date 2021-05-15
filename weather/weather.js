9cc9356420a33224e762eed19b1f8ddde3be0f39
Brihat Ratna Bajracharya <brihat.rb@gmail.com>
2021-05-15 18:49:08 +0545

    update weather on notify on each 5 minute mark + console log the status

--- weather/weather.js
+++ weather/weather.js
@@ -115,20 +115,24 @@ function get_weather() {
       document.getElementById("last_update").innerHTML = '<span class="width_240_data">' + updated_on.toLocaleDateString() + " </span>" + updated_on.toLocaleTimeString();
       document.getElementById("info").innerHTML = "";
       TIMEZONE = weather_data.timezone;
+      console.log("Updated Successfully on " + new Date());
     }
 
     else if (weather_data.cod == 404) {
       document.getElementById("info").style = "display: block;";
       document.getElementById("info").innerHTML = weather_data.message;
+      console.log("Update Failed on " + new Date());
     }
 
     else {
       alert("Unexpected Error");
+      console.log("Update Error on " + new Date());
     }
   };
 
   request.onerror = function() {
     document.getElementById("info").innerHTML = "network error";
+    console.log("No Update due to N/W Error on " + new Date());
   };
 
   // Send request
@@ -208,7 +212,6 @@ function show_weather_notification() {
 }
 
 function notifyMe() {
-  update_weather();
   if (!window.Notification) {
     console.log('This browser does not support notifications.');
   }
@@ -233,6 +236,12 @@ function notifyMe() {
       });
     }
   }
+
+  // UPDATE THE WEATHER ON EVERY MINUTE DIVISIBLE BY 5
+  if (new Date().getMinutes() % 5 == 0) {
+    update_weather();
+  }
+
   // check every minute, receive notification on every quarter of hour
   setTimeout("notifyMe()", 60000);
 }
