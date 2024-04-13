c888a10b44aae4bd81c67c4a8cd709620ceeb41d
Brihat Ratna Bajracharya <brihat.rb@gmail.com>
2024-04-13 07:54:31 +0545

    Update nepali_calendar_tools.js

--- calendar/nepali_calendar_tools.js
+++ calendar/nepali_calendar_tools.js
@@ -93,7 +93,7 @@ let select_list_ids = ["_", "_birthdate_", "_asof_", "_event_", "_date_calc_"];
 for (let elem = 0; elem < select_list_ids.length; elem++) {
     for (let year = 1975; year <= 2100; year++) {
         if (select_list_ids[elem] == "_event_") {
-            if (year < 2070 || year > 2080) {
+            if (year < 2070 || year > 2081) {
                 continue;
             }
         }
